cbb340cc77282ccf830d1fa5481f64dbe885be7d
Alison Joseph <alisonejoseph@gmail.com>
2018-10-21 07:07:59 -0500

    Remove hero text

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -6,10 +6,5 @@ import styles from './hero.module.css'
 export default ({ data }) => (
   <div className={styles.hero}>
     <Img className={styles.heroImage} alt={data.name} sizes={data.heroImage.sizes} />
-    <div className={styles.heroDetails}>
-      <h3 className={styles.heroHeadline}>{data.name}</h3>
-      <p className={styles.heroTitle}>{data.title}</p>
-      <p>{data.shortBio.shortBio}</p>
-    </div>
   </div>
 )
