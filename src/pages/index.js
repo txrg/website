9accb21190c4ec731aadd54bb9a52e7669987929
Alison Joseph <alisonejoseph@gmail.com>
2018-10-21 11:57:54 -0500

    remove person reference

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const posts = get(this, 'props.data.allContentfulBlogPost.edges')
-    const [author] = get(this, 'props.data.allContentfulPerson.edges')
 
     return (
       <div style={{ background: '#fff' }}>
