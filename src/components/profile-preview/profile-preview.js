92eddbc517da497a64be2c3852850d769e7fd401
Alison Joseph <alisonejoseph@gmail.com>
2018-12-30 23:29:28 -0600

    remove photo check since required now

--- src/components/profile-preview/profile-preview.js
+++ src/components/profile-preview/profile-preview.js
@@ -4,9 +4,7 @@ import Img from 'gatsby-image';
 export default ({ profile }) => (
   <div className="profile__preview  service-content">
     <div className="profile__thumbnail">
-    {profile.photo ? (
-          <Img alt={profile.name} fluid={profile.photo.fluid} />
-    ) : null}
+      <Img alt={profile.name} fluid={profile.photo.fluid} />
     </div>
     <h3 className="profile__name">{profile.name}</h3>
   </div>
