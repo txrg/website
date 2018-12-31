ef2abb3d70bc3a3929d465cf101c64b3481ab57a
Alison Joseph <alisonejoseph@gmail.com>
2018-12-31 01:36:08 -0600

    add photo back to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -181,7 +181,11 @@ export const pageQuery = graphql`
             }
           }
           featured
-          
+          photo {
+            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
+              ...GatsbyContentfulFluid_tracedSVG
+            }
+          }
         }
       }
     }
