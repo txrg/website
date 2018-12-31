5940f3498d8e2f11b93ec4094f07b08986f150bc
Alison Joseph <alisonejoseph@gmail.com>
2018-12-30 22:35:43 -0600

    if statement for skaters without photos

--- src/components/profile-preview/profile-preview.js
+++ src/components/profile-preview/profile-preview.js
@@ -4,7 +4,9 @@ import Img from 'gatsby-image';
 export default ({ profile }) => (
   <div className="profile__preview  service-content">
     <div className="profile__thumbnail">
-      <Img alt={profile.name} fluid={profile.photo.fluid} />
+    {profile.photo ? (
+          <Img alt={profile.name} fluid={profile.photo.fluid} />
+    ) : null}
     </div>
     <h3 className="profile__name">{profile.name}</h3>
   </div>
