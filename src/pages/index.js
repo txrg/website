eb156321e25267f1e9be0812ffc5f7ab0632e382
Alison Joseph <alisonejoseph@gmail.com>
2018-12-31 01:31:50 -0600

    remove from homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -181,11 +181,7 @@ export const pageQuery = graphql`
             }
           }
           featured
-          photo {
-            fluid(maxWidth: 350, maxHeight: 196, resizingBehavior: SCALE) {
-              ...GatsbyContentfulFluid_tracedSVG
-            }
-          }
+          
         }
       }
     }
