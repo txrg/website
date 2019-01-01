e632715e9671c92de8c9938ae0470352738f676f
Alison Joseph <alisonejoseph@gmail.com>
2018-12-31 19:31:54 -0600

    random update

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StaticQuery } from 'gatsby';
+import { StaticQuery, graphql } from 'gatsby';
 import Header from '../header/header';
 import Footer from '../footer/footer';
 import Helmet from 'react-helmet';
