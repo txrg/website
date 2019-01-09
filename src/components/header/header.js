6c214d993506d2f6b2800bbea24d849d28e9da71
Alison Joseph <alisonejoseph@gmail.com>
2019-01-09 17:18:39 -0600

    Fix header link

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -44,7 +44,7 @@ class Header extends React.Component {
             <a href="https://www.brownpapertickets.com/event/4044964" target="_blank" rel="noopener noreferrer"
               className="button button-primary cta">
               Buy Tickets
-            </Link>
+            </a>
           </nav>
 
           <button
