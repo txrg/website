5e9d7cb8638500e10a16029d312167cc3400e1dd
TXRGMarketing <116653169+TXRGMarketing@users.noreply.github.com>
2023-06-01 15:53:39 -0500

    Update header.js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -44,7 +44,7 @@ class Header extends React.Component {
               </li>
             </ul>
           
-            <a href="https://ticketbud.com/events/513ebe70-bd08-11ed-bb0b-42010a71701e" target="_blank" rel="noopener noreferrer"
+            <a href="https://ticketbud.com/events/ccd63a12-bd09-11ed-a99b-42010a71701e" target="_blank" rel="noopener noreferrer"
               className="button button-primary cta">
               Buy Tickets
             </a>
