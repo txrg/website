e36fe8a5c43f8293c03bd6838fe82318a256dd02
TXRGMarketing <116653169+TXRGMarketing@users.noreply.github.com>
2024-04-07 23:18:19 -0500

    2024 NA South Regionals roller derby in Texas page

--- src/components/regionals-nasouth-2024/navigation.js
+++ src/components/regionals-nasouth-2024/navigation.js
@@ -11,7 +11,7 @@ const Navigation = () => {
                 <Link to="/regionals-nasouth-2024/staff-and-volunteers" activeStyle={{ color: "#ffa926", fontWeight: "bolder" }}>Staff & Volunteers</Link>
                 <Link to="/regionals-nasouth-2024/sponsors-and-vendors" activeStyle={{ color: "#ffa926", fontWeight: "bolder" }}>Sponsors & Vendors</Link>
                 <Link to="/regionals-nasouth-2024/hospitality" activeStyle={{ color: "#ffa926", fontWeight: "bolder" }}>Hospitality</Link>
-                <Link to="/regionals-nasouth-2024/roller-derby-in-austin" activeStyle={{ color: "#ffa926", fontWeight: "bolder" }}>Roller Derby in Austin, TX</Link>
+                <Link to="/regionals-nasouth-2024/roller-derby-in-texas" activeStyle={{ color: "#ffa926", fontWeight: "bolder" }}>Roller Derby in Texas</Link>
             </div>
         </section>
     );
