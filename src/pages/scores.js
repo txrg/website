fb478eb18ea2a307a391b2b0836e165b76b72afc
TXRGMarketing <116653169+TXRGMarketing@users.noreply.github.com>
2024-11-18 20:41:01 -0600

    Add 2016 filter for scores page

--- src/pages/scores.js
+++ src/pages/scores.js
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout/layout';
 
-const FILTER_SEASON_YEARS = ['2024', '2023', '2020', '2019', '2018', '2017'];
+const FILTER_SEASON_YEARS = ['2024', '2023', '2020', '2019', '2018', '2017', '2016'];
 
 const ScoresPage = ({ data }) => {
     const [bouts, setBouts] = useState([]);
