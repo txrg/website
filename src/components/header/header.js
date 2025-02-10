dad362b48010e906a4bbd634400a23c15e73854b
TXRGMarketing <116653169+TXRGMarketing@users.noreply.github.com>
2025-02-10 01:34:23 -0600

    Add scores link to header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -27,6 +27,9 @@ const Header = () => {
               <Link to="/who-we-are/">Who we are</Link>
             </li>
             <li>
+              <Link to="/scores/">Scores</Link>
+            </li>
+            <li>
               <Link to="/events/">Events</Link>
             </li>
             <li>
