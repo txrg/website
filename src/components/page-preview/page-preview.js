2c3fc42befeeb562bacfd85ba8cb732c980ed4ab
TXRGMarketing <116653169+TXRGMarketing@users.noreply.github.com>
2025-02-12 15:33:18 -0600

    Fix feature page preview

--- src/components/page-preview/page-preview.js
+++ src/components/page-preview/page-preview.js
@@ -2,31 +2,31 @@ import React from 'react';
 import { Link } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 
-const PagePreview = ({ page }) => (
-  <div className="bgrid feature">
-    <div className="page__preview  service-content">
-      <Link
-        className="page__header"
-        to={
-          page.section === page.slug
-            ? `/${page.slug}/`
-            : `/${page.section
-                .toLowerCase()
-                .split(' ')
-                .join('-')}/${page.slug}/`
-        }>
-        <div className="page__thumbnail">
-          <GatsbyImage alt={page.title} image={page.featureImage.gatsbyImageData} />
-        </div>
-        <div className="feature__content">
-          <h4 className="feature__title">{page.title}</h4>
-          <p className="feature__description">
-            {page.featureDescription.childMarkdownRemark.rawMarkdownBody}
-          </p>
-        </div>
-      </Link>
+const PagePreview = ({ page }) => {
+  const sectionSlug = page.section.toLowerCase().split(' ').join('-');
+  return (
+    <div className="bgrid feature">
+      <div className="page__preview  service-content">
+        <Link
+          className="page__header"
+          to={
+            sectionSlug === page.slug
+              ? `/${page.slug}/`
+              : `/${sectionSlug}/${page.slug}/`
+          }>
+          <div className="page__thumbnail">
+            <GatsbyImage alt={page.title} image={page.featureImage.gatsbyImageData} />
+          </div>
+          <div className="feature__content">
+            <h4 className="feature__title">{page.title}</h4>
+            <p className="feature__description">
+              {page.featureDescription.childMarkdownRemark.rawMarkdownBody}
+            </p>
+          </div>
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default PagePreview;
