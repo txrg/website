c8a09d7e1a5eb5c55eb7da174163a206bd774665
TXRGMarketing <116653169+TXRGMarketing@users.noreply.github.com>
2025-04-03 23:06:35 -0500

    Add years to filter on scores page

--- src/pages/scores.js
+++ src/pages/scores.js
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import Layout from '../components/layout/layout';
 
-const FILTER_SEASON_YEARS = ['2025', '2024', '2023', '2020', '2019', '2018', '2017', '2016'];
+const FILTER_SEASON_YEARS = ['2025', '2024', '2023', '2020', '2019', '2018', '2017', '2016', '2015', '2014', '2013', '2012', '2011', '2010', '2009', '2008', '2007', '2006', '2005', '2004', '2003'];
 
 const ScoresPage = ({ data }) => {
     const [bouts, setBouts] = useState([]);
